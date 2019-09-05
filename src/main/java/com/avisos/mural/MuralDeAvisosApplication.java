e0f5d91a909332419ceaa35edb0f1fe612cc3065
Gisomar Junior <gisomar.silva@globalhitss.com.br>
2019-09-05 10:44:03 -0300

    expose id and cros origin

--- src/main/java/com/avisos/mural/MuralDeAvisosApplication.java
+++ src/main/java/com/avisos/mural/MuralDeAvisosApplication.java
@@ -1,7 +1,15 @@
 package com.avisos.mural;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 
 @SpringBootApplication
 public class MuralDeAvisosApplication {
@@ -9,5 +17,23 @@ public class MuralDeAvisosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MuralDeAvisosApplication.class, args);
 	}
+	
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.addAllowedMethod(HttpMethod.PUT);
+		config.addAllowedMethod(HttpMethod.DELETE);
+		config.addAllowedMethod(HttpMethod.GET);
+		config.addAllowedMethod(HttpMethod.POST);
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		
+		return bean;
+
+    }
+
 	
 }
--- src/main/java/com/avisos/mural/configuration/RepositoryConfiguration.java
+++ src/main/java/com/avisos/mural/configuration/RepositoryConfiguration.java
@@ -1,15 +1,26 @@
 package com.avisos.mural.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 import com.avisos.mural.notice.handler.NoticeEventHandler;
+import com.avisos.mural.notice.model.Notice;
 
 /**
  * Create Repository specific for Event Handler
  * @author Gisomar Jr.
  *
  */
-public class RepositoryConfiguration {
+@Configuration
+public class RepositoryConfiguration implements RepositoryRestConfigurer  {
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Notice.class);
+		RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config);
+	}
 
 	@Bean
 	NoticeEventHandler personEventHandler() {
